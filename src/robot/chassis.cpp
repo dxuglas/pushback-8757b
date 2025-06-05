a88c7deb64715359ac2c2163dbd9a62181e5d628
dxuglas <76638123+dxuglas@users.noreply.github.com>
2025-06-05 14:36:37 +1200

    Add tank drive function to chassis

--- src/robot/chassis.cpp
+++ src/robot/chassis.cpp
@@ -6,6 +6,11 @@ public:
         : l_motors(left_drive_motor_ports), r_motors(right_drive_motor_ports) {
     }
 
+    void tank(float left_joystick_y_position, float right_joystick_y_position) {
+        l_motors.move(left_joystick_y_position);
+        r_motors.move(right_joystick_y_position);
+    }
+
 private:
     pros::MotorGroup l_motors;
     pros::MotorGroup r_motors;
