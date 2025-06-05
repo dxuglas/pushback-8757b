5bf03d2a1f32ca127f7bd973f6a975817a4a9bf7
dxuglas <76638123+dxuglas@users.noreply.github.com>
2025-06-05 14:35:28 +1200

    Add constructor to chassis class which accepts drive motors

--- src/robot/chassis.cpp
+++ src/robot/chassis.cpp
@@ -1,6 +1,12 @@
+#include "pros/motor_group.hpp"
+
 class Chassis {
 public:
+    Chassis(std::initializer_list<int8_t> left_drive_motor_ports, std::initializer_list<int8_t> right_drive_motor_ports) 
+        : l_motors(left_drive_motor_ports), r_motors(right_drive_motor_ports) {
+    }
 
 private:
-
+    pros::MotorGroup l_motors;
+    pros::MotorGroup r_motors;
 };
