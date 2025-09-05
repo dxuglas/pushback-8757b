7efa33c6cb0bf8c58b53fedccece36e267007997
dxuglas <76638123+dxuglas@users.noreply.github.com>
2025-09-06 00:14:28 +1200

    removed drive smothing mode

--- include/utils/devices.h
+++ include/utils/devices.h
@@ -3,6 +3,7 @@
 
 #include "pros/adi.hpp"
 #include "pros/distance.hpp"
+#include "pros/optical.hpp"
 #include "pros/rotation.hpp"
 #include "robot/chassis.h"
 #include "robot/tracking/tracking_wheel.h"
@@ -10,12 +11,17 @@
 extern Chassis chassis;
 extern pros::Rotation left_h_rotation, right_h_rotation;
 extern TrackingWheel left_h_wheel, right_h_wheel, v_wheel;
-extern pros::IMU left_imu;
+extern pros::IMU imu;
 
-extern pros::Motor indexer, front_rollers, top_rollers, bin_control;
 extern pros::adi::Pneumatics match_load_ramp;
-extern pros::Distance block_detector;
-extern pros::Controller master;
+extern pros::Distance block_detector, center_goal_sensor;
+extern pros::Optical intake_sensor;
+extern pros::adi::DigitalIn long_goal_sensor;
+extern pros::adi::LineSensor left_line_sensor, right_line_sensor;
 extern pros::adi::DigitalOut indexer_led_left, indexer_led_right;
+
+extern pros::Motor indexer, intake_rollers, upper_stage_rollers, scoring_rollers;
+
+extern pros::Controller master;
 
 #endif
--- src/robot/chassis.cpp
+++ src/robot/chassis.cpp
@@ -15,8 +15,10 @@ Chassis::Chassis(std::initializer_list<int8_t> left_drive_motor_ports,
       r_deadzone(right_joystick_y_deadzone.value_or(0.0f)) {}
 
 void Chassis::tank(float left_joystick_y_position, float right_joystick_y_position) {
-    l_motors.move(check_threshold(left_joystick_y_position, l_deadzone));
-    r_motors.move(check_threshold(right_joystick_y_position, r_deadzone));
+    float left = check_threshold(left_joystick_y_position, l_deadzone);
+    float right = check_threshold(right_joystick_y_position, r_deadzone);
+    l_motors.move(left);
+    r_motors.move(right);
 }
 
 void Chassis::set_pose(float x, float y, float heading) {
